f4c311ddbe031d2d33ec1858933c7b97e1142571
Jefferson <jefferson.amsantos@hotmail.com>
2022-07-03 19:45:03 -0300

    Descricao criado

---
+++ CursoMVC/Models/Context.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CursoMVC.Models
+{
+    public class Context : DbContext
+    {
+        public DbSet<Categoria> Categorias { get; set; }//VAI TER UMA TABELA NO BD CHAMADA Categorias
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)//METODOS RESPONSAVEL POR CONFIGURAR O ENTITY FRAMEWORK - E FALAR QUAL BD QUE VAI ULTILIZAR
+        {
+            optionsBuilder.UseSqlServer(connectionString: @"Server=(localdb)\mssqllocaldb;Database=Cursomvc;Integrated Security=true");
+        }
+    }
+}
