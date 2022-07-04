c946497b42a05dc5bfccab6879e6d7dd8531615b
Jefferson <jefferson.amsantos@hotmail.com>
2022-07-03 23:05:27 -0300

    Produtos criado

--- CursoMVC/Models/Context.cs
+++ CursoMVC/Models/Context.cs
@@ -9,6 +9,7 @@ namespace CursoMVC.Models
     public class Context : DbContext
     {
         public DbSet<Categoria> Categorias { get; set; }//VAI TER UMA TABELA NO BD CHAMADA Categorias
+        public DbSet<Produto> Produtos { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)//METODOS RESPONSAVEL POR CONFIGURAR O ENTITY FRAMEWORK - E FALAR QUAL BD QUE VAI ULTILIZAR
         {
             optionsBuilder.UseSqlServer(connectionString: @"Server=(localdb)\mssqllocaldb;Database=Cursomvc;Integrated Security=true");
